79dcb6fccf7aa79aaecf37ce3343ff760f3cd86b
Sujish Lakshmanan <sujishtl@gmail.com>
2021-02-08 22:24:59 +1100

    more unit test cases

--- index.js
+++ index.js
@@ -1,73 +1,65 @@
 const rp = require('request-promise');
-var promise= require('promise');
 const cheerio = require('cheerio');
-const port = 3000;
 const fs = require("fs"),
-    { createCanvas } = require("canvas");
+	{ createCanvas } = require("canvas");
+
+let chartData = [];
+var chartNumData = [];
+
 var d = new Date,
-    dformat = [d.getDate(),
-			   d.getMonth()+1,
-               d.getFullYear()].join('-')+'_'+
-              [d.getHours(),
-               d.getMinutes(),
-			   d.getSeconds()].join('_');
+	dformat = [d.getDate(),
+	d.getMonth() + 1,
+	d.getFullYear()].join('-') + '_' +
+		[d.getHours(),
+		d.getMinutes(),
+		d.getSeconds()].join('_');
+main();
 
-let chartData =[];
-var chartNumData =[];
-let url= process.argv[2];
-console.log(`url is ${url}`);
+async function main() {
+	let url = process.argv[2];
+	console.log(`url is ${url}`);
 
-// if(isValidUrl(url)) {
-	getDataInArrayFromWebPage(url);
-// }
-// else {
-// 	console.log('Please provide a valid url');
-// }
-
-function isValidUrl(url)
-{
+	if (isValidUrl(url)) {
+		let data = await getDataFromWebPage(url)
+		chartNumData = getDataOfWebPageInArray(data);
+		createImage(chartNumData);
+	}
+	else {
+		console.log('Please provide a valid url');
+	}
+}
+function isValidUrl(url) {
 	var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
 	var regex = new RegExp(expression);
-	if (url.match(regex)) {
-	  console.log("Successfully matched URL Pattern");
-	  return true;	  
+	if (typeof url !== 'undefined' && url.match(regex)) {
+		console.log("Successfully matched URL Pattern");
+		return true;
 	} else {
-	  console.log("URL Pattern is not matching");
-	  return false;
+		console.log("URL Pattern is not matching");
+		return false;
 	}
-	
+
 }
 
-function getDataInArrayFromWebPage(url) {	
+function getDataFromWebPage(url) {
+	var options = {
+		url: url,
+		transform: body => cheerio.load(body)
+	}
+	return rp(options)
+}
 
-			var options = {
-			//	url: 'https://en.wikipedia.org/wiki/List_of_highest_individual_scores_in_One_Day_International_cricket',
-				url:url,
-				transform: body => cheerio.load(body)
-			}
-			
-			rp(options)
-				.then(function ($) {
-					 htmlpage= $.html();
-			$('td').each(function () {
-				if ($(this).html().match(/^\s*\d[\d,\.]*\s*$/)) {
-					$(this).css('background-color', 'red');					
-					chartData.push($(this).text());
-				}
-			});
-			for (let data of chartData) {
-			 	chartNumData.push(parseFloat(data));
-			}
-			chartNumData=chartNumData.sort((a, b) => a - b);
-			console.log("Inside rp"+chartNumData.length);
-		})
-		.then(()=>{
-				console.log("Inside secound then"+chartNumData.length);
-				createImage(chartNumData);
-		});	
-
-	
-	
+function getDataOfWebPageInArray($) {
+	$('td').each(function () {
+		if ($(this).html().match(/^\s*\d[\d,\.]*\s*$/)) {
+			chartData.push($(this).text());
+		}
+	});
+	for (let data of chartData) {
+		chartNumData.push(parseFloat(data));
+	}
+	chartNumData = chartNumData.sort((a, b) => a - b);
+	return chartNumData;
 }
 
 
@@ -79,19 +71,19 @@ function createImage(dataFeed) {
 	ctx.fillRect(0, 0, WIDTH, HEIGHT);
 	ctx.fillStyle = "#f2f2f2";
 	ctx.font = "12px Arial";
-	let y=10;
-	for ( let d of dataFeed) {
+	let y = 10;
+	for (let d of dataFeed) {
 		var x;
-		let line='|';
-		 for ( x=0; x<d; x++)
-		 {
-            line = line +'|';
-		 }
-		ctx.fillText(d+line,5,y);
-		y=y+10;
+		let line = '|';
+		for (x = 0; x < d; x++) {
+			line = line + '|';
+		}
+		ctx.fillText(d + line, 5, y);
+		y = y + 10;
 	}
 	const buffer = canvas.toBuffer("image/png");
 	fs.writeFileSync(`Pic_${dformat}.png`, buffer);
+	console.log(`Image saved as Pic_${dformat}.png`)
 }
 
-module.exports = { getDataInArrayFromWebPage: getDataInArrayFromWebPage, createImage: createImage,isValidUrl:isValidUrl} 
+module.exports = { getDataOfWebPageInArray: getDataOfWebPageInArray, createImage: createImage, isValidUrl: isValidUrl, getDataFromWebPage: getDataFromWebPage } 
--- index.test.js
+++ index.test.js
@@ -1,13 +1,42 @@
 const index = require('./index');
+const fs = require("fs");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(index.sum(1, 2)).toBe(3);
-});
+
+//const $ =jest.fn(() => ({ each: jest.fn()}));
+const mockDataFn= () => {
+  //var buffer= fs.readFileSync('./Pic_8-2-2021_17_41_25.txt');
+// return Array.prototype.slice.call(buffer, 0);
+ return 1;
+
+};
+//$.each.mock= 
+ //const $ = jest.fn();
+ const $ =jest.fn(() => ({ each: jest.fn(e => {return  fs.readFileSync('./Pic_8-2-2021_17_41_25.txt')})}))
+
+//$.mockReturnValue(mockDataFn());
+
+
+
 
 test('123 is not valid url', () => {
+  console.log("Mock type "+ typeof mockDataFn() );
     expect(index.isValidUrl('123')).toBe(false);
   });
 
-  test('http://www.google.com is a valid url', () => {
+test('http://www.google.com is a valid url', () => {
     expect(index.isValidUrl('http://www.google.com')).toBe(true);
   });
+
+
+
+   test('getDataInArrayFromWebPage function returns array', () => {
+     
+    expect( Array.isArray(index.getDataOfWebPageInArray($))).toBe(true);
+   });
+
+   test('getDataInArrayFromWebPage function returns defined', () => {
+    expect(index.getDataOfWebPageInArray($)).toBeDefined();
+  });
+  
+
+
